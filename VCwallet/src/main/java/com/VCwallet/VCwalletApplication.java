0a480cadf6b83b409c786748ed044c25971564f9
lambao <baolam12122004@gmail.com>
2024-11-16 20:10:14 +0700

    first commit

---
+++ VCwallet/src/main/java/com/VCwallet/VCwalletApplication.java
@@ -0,0 +1,12 @@
+package com.VCwallet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VCwalletApplication.class, args);
+	}
+
+}
---
+++ VCwallet/src/test/java/com/Myproject/VCwallet/VCwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Myproject.VCwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
